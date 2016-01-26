5facd1a134987a7ff968261f831cdc791e490670
Ben Le Cam <blecam@four51.com>
2016-01-26 11:33:05 -0600

    Fixing stuff

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if(substr($text, 0, 3) == "add")
     $reply = "Thanks for sharing, mate!";
 } else {
     $randomInt = rand(0, count($quotes));
-    $reply = '*'.$quotes[$randomInt].'*';
+    $reply = '*'.str_replace('\n', '', $quotes[$randomInt]).'*';
 }
 
 echo $reply;
