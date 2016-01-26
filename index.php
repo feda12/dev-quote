d3f16e7d36c5894b276f34440d09f5c66de47b7f
Ben Le Cam <blecam@four51.com>
2016-01-26 10:37:39 -0600

    devs quotes'

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
 
-/*
-
-REQUIREMENTS
-
-* A custom slash command on a Slack team
-* A web server running PHP5 with cURL enabled
-
-USAGE
-
-* Place this script on a server running PHP5 with cURL.
-* Set up a new custom slash command on your Slack team:
-  http://my.slack.com/services/new/slash-commands
-* Under "Choose a command", enter whatever you want for
-  the command. /isitup is easy to remember.
-* Under "URL", enter the URL for the script on your server.
-* Leave "Method" set to "Post".
-* Decide whether you want this command to show in the
-  autocomplete list for slash commands.
-* If you do, enter a short description and usage hint.
-
-*/
-session_start();
-
-$Unknown = 0;
-$Out = 1;
-$Low = 2;
-$In = 3;
-
 # Grab some of the values from the slash command, create vars for post back to Slack
 $command = $_POST['command'];
 $text = $_POST['text'];
@@ -40,52 +12,26 @@ if($token != getenv("SLACK_TOKEN")){ #replace this with the token from your slas
   echo $msg;
 }
 
-$snacks = $Unknown;
 
-$file = 'snack.txt';
+$file = 'quotes.txt';
 if (!file_exists($file)) {
         touch($file);
 }
 $handle = fopen($file, "r+");
-if(flock($handle, LOCK_EX)) {
-    $size = filesize($file);
-    $snacks = $size === 0 ? 0 : fread($handle, $size);
-    flock($handle, LOCK_UN);
-}
+$quotes = file($file)
 
-if($text == "status")
+if(sustr($text, 0, 2) == "add")
 {
-        if($snacks == $In)
-        {
-            $reply = "Yay! Snacks everywhere!";
-        } else if ($snacks == $Low) {
-            $reply = "We are running low... let Karie know before it's too late..";
-        } else if ($snacks == $Out) {
-            $reply = "We're out... Should I file a bug?";
-        } else {
-            $reply = "404: Snacks not found";
-        }
-} else {
-    $status = $Unknown;
-    $reply = "404: Snacks not found";
-    if($text == "in")
-    {
-        $status = $In;
-        $reply = "Snacks have arrived! Hallelujah!";
-    } else if ($text == "low") {
-        $status = $Low;
-        $reply = "Holy cow! Someone shoud call Karie!!";
-    } else if ($text == "out") {
-        $status = $Out;
-        $reply = "Argh. No more snacks";
-    }
+    $handle = fopen($file, "r+");
     if(flock($handle, LOCK_EX)) {
         ftruncate($handle, 0);
         rewind($handle);
         fwrite($handle, $status);
         flock($handle, LOCK_UN);
     }
+    $reply = "Thanks for sharing, mate!"
+} else {
+    $reply = $quotes[rand(0, count($quotes)];
 }
 
-# Send the reply back to the user.
 echo $reply;
